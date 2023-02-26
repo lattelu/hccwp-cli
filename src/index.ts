cd4c483cb881b6c0202cc88e2e9cfd09e0916737
agan <1094143007@qq.com>
2023-02-26 20:58:08 +0800

    fix(name): 修复package获取问题
    undefined

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,36 @@
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
 import fs from 'fs-extra'
 import chalk from 'chalk'
 import { program } from 'commander'
 import figlet from 'figlet'
 import { create } from './core/index.js'
 import type { CreateOptions, PackageJSON } from './types.js'
+import { isValidPackageName, toValidPackageName } from './utils/index.js'
 
-const packageJSON = fs.readJSONSync('./package.json') as PackageJSON
+const __filename = fileURLToPath(import.meta.url)
+const packagePath = path.resolve(__filename, '../../package.json')
+
+const packageJSON = fs.readJSONSync(packagePath) as PackageJSON
 
 program
   .command('create <name>')
   .description('创建一个系统项目')
   .option('-f, --force', 'overwrite target directory if it exist')
   .action(async (name: string, options: CreateOptions) => {
-    await create(name, options)
+    const packageName = toValidPackageName(name)
+    if (isValidPackageName(packageName)) {
+      await create(name, options)
+    } else {
+      console.log(`${chalk.bgRed(`${name} 不符合 package name 标准`)}`)
+    }
+
     // 打印命令行输入的值
   })
 
 program
   // 配置版本号信息
+  .name('hccwp-cli')
   .version(`v${packageJSON.version}`)
   .usage('<command> [option]')
 
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,15 @@
 export const REPO_NAME = 'zhurong-cli'
+export function isValidPackageName(projectName: string) {
+  return /^(?:@[a-z0-9-*~][a-z0-9-*._~]*\/)?[a-z0-9-~][a-z0-9-._~]*$/.test(
+    projectName,
+  )
+}
+
+export function toValidPackageName(projectName: string) {
+  return projectName
+    .trim()
+    .toLowerCase()
+    .replace(/\s+/g, '-')
+    .replace(/^[._]/, '')
+    .replace(/[^a-z0-9-~]+/g, '-')
+}

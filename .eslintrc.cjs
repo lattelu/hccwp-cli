d3fb1e533cafacf92bdffd12049629ca58d5d462
agan <1094143007@qq.com>
2023-02-26 18:02:21 +0800

    chore(config): :wrench: change config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,20 @@
-process.env.ESLINT_TSCONFIG = "tsconfig.json";
+process.env.ESLINT_TSCONFIG = 'tsconfig.json'
 
 module.exports = {
-	env: {
-		es2021: true,
-		node: true,
-	},
-	extends: "@antfu",
-	overrides: [],
-	parserOptions: {
-		ecmaVersion: "latest",
-		sourceType: "module",
-	},
-	rules: {},
-};
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['@antfu', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'antfu/if-newline': 'off',
+    'no-console': 'off',
+  },
+}
